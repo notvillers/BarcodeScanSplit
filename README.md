5bf515f8dc81705498d671e158badaf680140725
Villers <117743062+notvillers@users.noreply.github.com>
2024-11-12 23:55:55 +0100

    config + md

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-zbar, poppler
+# BarcodeScanSplit
+
+## Extra requirements
+- [Zbar](https://github.com/NaturalHistoryMuseum/pyzbar)
+- [Poppler](https://poppler.freedesktop.org/) or [Poppler for Windows](https://github.com/oschwartz10612/poppler-windows)
+
+## Setup
+Modify the directories in the [config.py](config.py) if needed.
+
+## Run
+### Windows
+```
+.\start.bat
+```
--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-'''config file'''
+'''This module handles the creation of necessary directories for the BarcodeScanSplit project.'''
 
 import os
 
@@ -9,26 +9,34 @@ def make_dir(dir_path: str):
 
 path: str = os.path.dirname(__file__)
 
+# Directories
+
+# Log directory
 LOG_DIR: str = "logs"
 log_path: str = os.path.join(path, LOG_DIR)
 make_dir(log_path)
 
+# Document directory
 DOC_DIR: str = "docs"
 doc_path: str = os.path.join(path, DOC_DIR)
 make_dir(doc_path)
 
+# Temp directory
 TEMP_DIR: str = "temp"
 temp_path: str = os.path.join(path, TEMP_DIR)
 make_dir(temp_path)
 
+# Image directory
 IMG_DIR: str = "images"
 img_path: str = os.path.join(path, IMG_DIR)
 make_dir(img_path)
 
+# Output directory
 OUTPUT_DIR: str = "out"
 output_path: str = os.path.join(path, OUTPUT_DIR)
 make_dir(output_path)
 
+# Backup directory
 BACKUP_DIR: str = "backup"
 backup_path: str = os.path.join(path, BACKUP_DIR)
 make_dir(backup_path)
